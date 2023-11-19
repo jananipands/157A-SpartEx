19dd65594d26e387a33432e9ef467ae0d0ff613d
Janani Pandurangan <janani.pandurangan@sjsu.edu>
2023-11-18 21:53:14 -0800

    Created initial project

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get("/", function(req, res){
+    res.send("<h1>Things are working!</h1>");
+})
+
+
+// starts the server via express on Port 3000
+app.listen(3000, function(){
+    console.log("Server running on Port 3000!");
+});
